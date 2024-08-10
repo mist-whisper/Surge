007427a70965eaa48a5cbc47c3e1ec11f6443f37
mist-whisper <114045385+mist-whisper@users.noreply.github.com>
2024-08-10 19:17:59 +0800

    更新 Yangshipin.js

--- Scripts/Yangshipin/Yangshipin.js
+++ Scripts/Yangshipin/Yangshipin.js
@@ -6,7 +6,7 @@
 #广告
 ^https?:\/\/cdn\.cmgadx\.com\/sdk\/pool\/.+\.json url reject-dict
 #会员
-^http:\/\/(liveinfo|bkliveinfo|playvv)\.ysp\.cctv\.cn\/(playvinfo\?.+|.*) url script-request-header ysp.js
+^http:\/\/(liveinfo|bkliveinfo|playvv)\.ysp\.cctv\.cn\/(playvinfo\?.+|.*) url script-request-header Yangshipin.js
 
 [mitm]
 thostname = *.ysp.cctv.cn, cdn.cmgadx.com
